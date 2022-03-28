153be276384cf94b5e479637d047218eaf02008b
tashton <ta@shpock.com>
2022-03-28 11:55:00 +0200

    add reference test

---
+++ tests/Model/ReferenceTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Finderly\OpenapiPhpLib\Model;
+
+use Finderly\OpenapiPhpLib\Model\Component\Components;
+use Finderly\OpenapiPhpLib\Model\Path\PathItem;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * 
+ */
+class ReferenceTest extends TestCase
+{
+    public function testInstantiation(): void
+    {
+        $obj = new Reference();
+
+        $this->assertTrue($obj instanceof Reference);
+    }
+
+    public function testIsArrayable(): void
+    {
+        $obj = new Reference();
+
+        $this->assertTrue($obj instanceof ArrayableInterface);
+        $this->assertTrue(method_exists($obj, 'toArray'));
+    }
+
+    public function testReferenceSetRef(): void
+    {
+        $obj = Reference::create('test.reference', 'override/');
+        $this->assertEquals('override/test.reference', $obj->getRef());
+
+        $obj = Reference::create('test.reference');
+        $this->assertEquals('#/components/schemas/test.reference', $obj->getRef());
+
+        $obj = Reference::create('test.reference', null);
+        $this->assertEquals('test.reference', $obj->getRef());
+    }
+
+    public function testObjectToArray(): void
+    {
+        $obj = Reference::create('test.reference');
+        $this->assertEquals(
+            ['$ref' => '#/components/schemas/test.reference'],
+            $obj->toArray()
+        );
+    }
+}
